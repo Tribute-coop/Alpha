aa51e5628a6464e42f8adcb1c0850950b72bc694
Borja Ruiz Torres <borja.epsc@gmail.com>
2019-05-11 19:02:02 +0200

    Use form-group in SearchSelect

--- client/src/app/shared/search-select/search-select.tsx
+++ client/src/app/shared/search-select/search-select.tsx
@@ -6,7 +6,7 @@ export function SearchSelect(props: HTMLProps<HTMLSelectElement>): JSX.Element {
   const { name, value, onChange, children, label } = props;
 
   return (
-    <div className="search-select">
+    <div className="search-select form-group">
       { label && <label className="search-select__label uppercase-label">{label}:</label> }
       <select name={name} className="search-select__select form-control"
         onChange={onChange} value={value}>
