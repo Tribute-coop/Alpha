c55dfe8d3e9c904840617a4b80b3d902cdb2730b
Borja Ruiz Torres <borja.epsc@gmail.com>
2019-07-04 20:26:08 +0200

    Simplify svg files

--- client/src/app/core/layout/nav-bar/icons/bolt-icon.tsx
+++ client/src/app/core/layout/nav-bar/icons/bolt-icon.tsx
@@ -3,15 +3,9 @@ import React from 'react';
 export function BoltIcon(): JSX.Element {
   return (
     <svg version="1.1" viewBox="0 0 10 17" xmlns="http://www.w3.org/2000/svg">
-      <g fill="none" fillRule="evenodd" opacity=".8">
-        <g transform="translate(-23 -716)" stroke="currentColor" strokeWidth="1">
-          <g transform="translate(-16 -11)">
-            <g transform="translate(16 12)">
-              <g transform="translate(15 716)">
-                <polygon points="10 16 12 9 8.75 9 11 0 17 0 13 6 17 6"/>
-              </g>
-            </g>
-          </g>
+      <g fill="none" fillRule="evenodd">
+        <g transform="translate(-8 1)" stroke="currentColor" strokeWidth="1">
+          <polygon points="10 16 12 9 8.75 9 11 0 17 0 13 6 17 6"/>
         </g>
       </g>
     </svg>
