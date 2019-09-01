4ef4cb6d267bad24cce0a19eb7dd353672cbba2e
Borja Ruiz Torres <borja.epsc@gmail.com>
2019-09-01 21:29:10 +0200

    Fix NotFound component if project id doesnt exist

--- client/src/app/project/project-layout.tsx
+++ client/src/app/project/project-layout.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { RouteComponentProps } from 'react-router';
+import { Link } from 'react-router-dom';
 
 import { RouterOutlet } from 'app/shared/components/router-outlet';
+import { NotFound } from 'app/shared/components';
 import { useTitleFromPath } from 'app/shared/hooks';
 import { ProjectSelector } from './project-selector';
 import routes from './project.routes';
 
+import { projects as mockProjects } from '../mocks';
+
 export function ProjectLayout(props: RouteComponentProps): JSX.Element {
   const { match, location } = props;
+  const { id } = match.params as { id: string };
   const { t } = useTranslation();
   const title = useTitleFromPath(location.pathname);
   const [ showNewContribution, setShowNewContribution ] = useState<boolean>(false);
@@ -17,6 +21,14 @@ export function ProjectLayout(props: RouteComponentProps): JSX.Element {
   useEffect((): void => {
     setShowNewContribution(location.pathname.includes('assignments'));
   }, [location.pathname]);
+
+  const invalidProjectId = mockProjects.every(
+    project => project.id !== id
+  );
+
+  if (invalidProjectId) {
+    return <NotFound />;
+  }
 
   return (
     <div className="main-layout">
@@ -26,7 +38,8 @@ export function ProjectLayout(props: RouteComponentProps): JSX.Element {
         <h4 className="main-layout__title">{t(title)}</h4>
 
         { showNewContribution &&
-          <Link className="btn btn-primary" to={match.url + '/contributions/assignments/new'}>
+          <Link className="btn btn-primary"
+            to={match.url + '/contributions/assignments/new'}>
             {t('project.contributions.newContribution')}
           </Link>
         }
